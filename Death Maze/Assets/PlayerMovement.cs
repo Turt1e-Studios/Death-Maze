226d1c720738d7252af711789c15503c5b151115
Aishu85 <aishu.aravind5@gmail.com>
2022-10-15 18:01:03 -0700

    Chaser Movement

--- Death Maze/Assets/PlayerMovement.cs
+++ Death Maze/Assets/PlayerMovement.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+
 {
+    float speed = 10;
+
     // Start is called before the first frame update
     void Start()
+
     {
         
     }
@@ -13,6 +17,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 pinput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = pinput.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        transform.position += moveAmount;
         
     }
 }
